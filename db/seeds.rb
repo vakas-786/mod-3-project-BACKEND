6f7f36f9a9412f0f2a476e244132a9d331d4d4f3
vakas-786 <vakas0959@gmail.com>
2020-08-17 11:02:31 -0400

    setting up database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+ANIMAL_URL = 'https://acnhapi.com/v1a/villagers/'
+
+def get_animals(animal_url)
+    uri = URI.parse(animal_url)
+    response = NET::HTTP.get_response(uri)
+    animal_obj = JSON.parse(response.body)
+    animal_obj.each do |characters|
+        characters.create(name: characters[])
+    end 
+end 
+
