7c839bcf51baa9819298ff26176a9d30db77ab52
vakas-786 <vakas0959@gmail.com>
2020-08-17 16:12:32 -0400

    finished database and api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,33 @@
+require 'net/http'
 
-
+Comment.destroy_all
+Animal.destroy_all 
+User.destroy_all 
 
 ANIMAL_URL = 'https://acnhapi.com/v1a/villagers/'
 
 def get_animals(animal_url)
     uri = URI.parse(animal_url)
-    response = NET::HTTP.get_response(uri)
+    response = Net::HTTP.get_response(uri)
     animal_obj = JSON.parse(response.body)
+    # byebug 
     animal_obj.each do |characters|
-        characters.create(name: characters[])
+        # byebug
+        name = characters["name"]["name-USen"]
+        Animal.create(name: name, image_url: characters["image_uri"], personality: characters["personality"], quote: characters["saying"], icon_url: characters["icon_uri"], species: characters["species"])
     end 
+    # byebug
 end 
+
+get_animals(ANIMAL_URL) 
+
+User.create(name: "User 1")
+
+# 100.times do 
+# Comment.create({animal_id: Animal.all.sample.id, user_id: User.all.sample.id, text: "hello"})
+# end 
+
+
+
+
 
