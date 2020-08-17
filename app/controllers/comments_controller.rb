19a8ed4e9f7cd553c388bf08203524d349962e20
vakas-786 <vakas0959@gmail.com>
2020-08-17 17:21:03 -0400

    message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+    def index 
+        comments = Comment.all 
+        render json: comments, only: [:id, :animal_id, :user_id, :text]
+    end 
+
+    def show 
+        comment = Comment.find_by(id: params[:id])
+        render json: comment, only: [:id, :animal_id, :user_id, :text]
+    end 
 end
